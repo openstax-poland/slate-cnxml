b22c230a0342099bc9c48d6a9305a0f5fa244ad6
piotr@obrandowani.pl <piotr@obrandowani.pl>
2021-06-11 11:33:31 +0200

    Add test for list in para in exercise in section

--- test/de/section.js
+++ test/de/section.js
@@ -1,31 +1,42 @@
 /** @jsx h */
 
 export const input = cnxml`
-<section id="s1">
-    <title>Section</title>
-    <para>This is a section</para>
-    <section id="s2">
-        <title>Another section</title>
-        <para>This is a sub-section</para>
-    </section>
-</section>
 <section id="s3">
     <title>One more section</title>
-    <para>Only sections can follow sections</para>
+    <exercise>
+        <problem>
+            <para>Only sections can follow sections
+                <list>
+                    <item>item</item>
+                </list>
+            </para>
+        </problem>
+    </exercise>
 </section>
 `
 
 export const output = <document>
-    <section id="s1">
-        <title>Section</title>
-        <p>This is a section</p>
-        <section id="s2">
-            <title>Another section</title>
-            <p>This is a sub-section</p>
-        </section>
-    </section>
     <section id="s3">
         <title>One more section</title>
-        <p>Only sections can follow sections</p>
+        <exercise>
+            <exproblem>
+                <p>Only sections can follow sections </p>
+                <itemlist>
+                    <li><p>item</p></li>
+                </itemlist>
+                <p><text/></p>
+            </exproblem>
+        </exercise>
     </section>
 </document>
+
+export const errors = [
+    [
+      "unexpected-element",
+      {
+        "id": null,
+        "localName": "list",
+        "namespace": "http://cnx.rice.edu/cnxml",
+      }
+    ]
+]
