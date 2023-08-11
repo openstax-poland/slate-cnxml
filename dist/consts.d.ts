b5e4013a6f493eeb953a33559e7473a69d70c3dd
Krzysztof Mędrzycki <aiwenari@gmail.com>
2023-08-11 11:41:15 +0200

    Publish slate-cnxml 0.2.4-pre.8

---
+++ dist/consts.d.ts
@@ -0,0 +1,8 @@
+export declare const XML_NAMESPACE = "http://www.w3.org/XML/1998/namespace";
+export declare const XMLNS_NAMESPACE = "http://www.w3.org/2000/xmlns/";
+export declare const CNXML_NAMESPACE = "http://cnx.rice.edu/cnxml";
+/** Natural language extensions to CNXML */
+export declare const CMLNLE_NAMESPACE = "http://katalysteducation.org/cmlnle/1.0";
+export declare const CXLXT_NAMESPACE = "http://katalysteducation.org/cxlxt/1.0";
+/** CNXML extensions to facilitate better editing experience */
+export declare const EDITING_NAMESPACE = "http://adaptarr.naukosfera.com/editing/1.0";
